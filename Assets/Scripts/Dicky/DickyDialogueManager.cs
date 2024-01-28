24567c348446b41a8b7b5f0d66e80ac3246b6dc7
Tate Donnelly <58042977+Tate-Donnelly@users.noreply.github.com>
2024-01-28 13:36:34 -0500

    Dicky's reactions + flag system

--- Assets/Scripts/Dicky/DickyDialogueManager.cs
+++ Assets/Scripts/Dicky/DickyDialogueManager.cs
@@ -137,6 +137,7 @@ namespace Dicky
             else
             {
                 reaction = reactionQue.Dequeue();
+                print(reaction.ToString());
                 reactionQue.Clear();
             }
             
@@ -189,16 +190,31 @@ namespace Dicky
     public class DickyReaction
     {
         public List<DialogueSO> reactionDialogue=new List<DialogueSO>();
-        private Queue<DialogueSO> reactionQue;
+        private Queue<DialogueSO> reactionQue=new Queue<DialogueSO>();
 
         void Awake()
         {
             reactionQue = new Queue<DialogueSO>(reactionDialogue);
+            foreach (DialogueSO dialogueSo in reactionDialogue)
+            {
+                reactionQue.Enqueue(dialogueSo);
+            }
+            Debug.Log(reactionDialogue.Count);
             Debug.Log(reactionQue.Count);
+        }
+
+        void LoadQue()
+        {
+            reactionQue = new Queue<DialogueSO>(reactionDialogue);
+            foreach (DialogueSO dialogueSo in reactionDialogue)
+            {
+                reactionQue.Enqueue(dialogueSo);
+            }
         }
 
         public DialogueSO GetReaction()
         {
+            if (reactionQue.Count == 0) LoadQue();
             return reactionQue.Dequeue();
         }
     }
